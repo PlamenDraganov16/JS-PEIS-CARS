ad324ba03a77565930d1abfad04eb7c5312a9962
Plamen Draganov <159624750+PlamenDraganov16@users.noreply.github.com>
2025-04-08 11:21:08 +0300

    Add files via upload

---
+++ models/buy.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const purchaseSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    }
+});
+
+const Purchase = mongoose.model('Purchase', purchaseSchema, 'orders');
+module.exports = Purchase;
