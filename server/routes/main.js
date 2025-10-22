46ef2d8aae5999914e0f5702b627d7015e7baeec
Plamen Draganov <plamine95@gmail.com>
2025-10-22 21:32:37 +0300

    remade some more functions into main.js for smaller routers

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,118 +1,83 @@
-import Review from '../models/review.js';
-import Car from '../models/car.js';
-import Purchase from '../models/buy.js';
 import express from 'express';
-
+import Purchase from '../models/buy.js';
 import getErrorMessage from '../utils/errorUtils.js';
+import { getCars, getCarsById } from '../services/carService.js';
 
 const router = express.Router();
 
 
 // GET HOME **
-
-router.get('/', (req, res) => {
-    res.redirect('./home');
-})
-
-// GET HOME **
-
-router.get('/home', (req, res) => {
-    res.render('index', {title: 'Welcome'});
-})
+router.get('/', (req, res) => { res.redirect('./home') });
+router.get('/home', (req, res) => { res.render('index', { title: 'Welcome' }) });
 
 // GET ABOUT **
-
 router.get('/about', (req, res) => {
-    res.render('about', {title: 'About us'});
-})
+    res.render('about', { title: 'About us' });
+});
 
 // GET CATALOGUE **
-
 router.get('/catalogue', async (req, res) => {
     try {
         const searchTerm = req.query.search;
-        let cars = [];
-
-        if (searchTerm) {
-            cars = await Car.find({ name: { $regex: searchTerm, $options: 'i' } }); 
-        } else {
-            cars = await Car.find().sort({ createdAt: -1 });
-        }
-
+        const cars = await getCars(searchTerm);
         res.render('catalogue', { cars, title: "Our Cars" });
-
     } catch (err) {
         console.log(err);
-        res.status(500).send("Server Error");
+        res.status(500).send(getErrorMessage(err));
     }
 });
 
 // GET CAR DETAILS **
-
-router.get('/catalogue/:id', (req, res) => {
-    const id = req.params.id;
-
-    Car.findById(id)
-    .then(carResult => {
-        Review.find().sort({ createdAt: -1 })
-        .then(reviewResult => {
-            res.render('details', { 
-                car: carResult, 
-                title: 'Details', 
-                reviews: reviewResult 
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send("Error fetching reviews");
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).send("Error fetching car details");
-    });
+router.get('/catalogue/:id', async (req, res) => {
+  try {
+    const { car, reviews } = await getCarsById(req.params.id);
+    res.render('details', { car, reviews, title: 'Details' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(getErrorMessage(err));
+  }
 });
 
 // GET LOGIN PAGE **
 
 router.get('/login', async (req, res) => {
     try {
-    res.render('admin/login', {title: 'Login'});
+        res.render('admin/login', { title: 'Login' });
     } catch (err) {
         console.log(err);
         res.status(404);
     }
-  });
+});
 
 // POST REVIEWS AND PURCHASE **
 
 router.post('/catalogue/:id', (req, res) => {
-  
-   if (req.body.formType === 'purchase') {
+
+    if (req.body.formType === 'purchase') {
         const newPurchase = new Purchase(req.body);
 
         newPurchase.save()
-        .then(() => {
-            res.redirect('/catalogue');
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send("Error making the purchase");
-        })
+            .then(() => {
+                res.redirect('/catalogue');
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).send("Error making the purchase");
+            })
 
     } else if (req.body.formType === 'review') {
 
         const newReview = new Review(req.body)
 
         newReview.save()
-        .then(() => {
-        res.redirect('/catalogue');
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send("Error making the review");
-        })
-        
+            .then(() => {
+                res.redirect('/catalogue');
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).send("Error making the review");
+            })
+
 
     } else {
         res.status(400).send("Invalid form submission");
--- server/services/carService.js
+++ server/services/carService.js
@@ -4,7 +4,7 @@ import Review from '../models/review.js'
 export async function getCars(searchTerm) {
     if (searchTerm) return await Car.find({ name: { $regex: searchTerm, $options: 'i' } });
 
-    return (await Car.find()).sort({ createdAt: -1 });
+    return await Car.find().sort({ createdAt: -1 });
 };
 
 export async function getCarsById(id) {
@@ -12,7 +12,7 @@ export async function getCarsById(id) {
 
     if (!car) throw new Error('Car not found!');
 
-    const reviews = (await Review.find()).toSorted({ createdAt: -1 });
+    const reviews = await Review.find().sort({ createdAt: -1 });
 
     return { car, reviews };
 }
